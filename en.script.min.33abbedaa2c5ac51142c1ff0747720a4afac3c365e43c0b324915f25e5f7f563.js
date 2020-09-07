2fb3768c4d1b9e5199318a4eaa04ad07e2e2f2cf
webninjasi <webninjasi@users.noreply.github.com>
2020-09-07 12:09:17 +0000

    Update

--- en.script.min.33abbedaa2c5ac51142c1ff0747720a4afac3c365e43c0b324915f25e5f7f563.js
+++ en.script.min.33abbedaa2c5ac51142c1ff0747720a4afac3c365e43c0b324915f25e5f7f563.js
@@ -1,4 +1,4 @@
-$(function(){var baseURL="https://webninjasi.github.io/tfmrecords/";var maps={"1234":1,};$('.date-convert').each(function(){$(this).text(moment.unix($(this).text()).format("LL"));});$("#book-search-input").keypress(function(e){if(e.keyCode!=13){return true;}
+$(function(){var baseURL="https://webninjasi.github.io/tfmrecords/";var maps={"1":1,"1234":1,"123456":1,};$('.date-convert').each(function(){$(this).text(moment.unix($(this).text()).format("LL"));});$("#book-search-input").keypress(function(e){if(e.keyCode!=13){return true;}
 var mapid=parseInt($(this).val().trim().replace('@',''));if(isNaN(mapid)){$("#book-search-results").text("Map ID is invalid!");return false;}
 if(!(mapid in maps)){$("#book-search-results").text("Map not found!");return false;}
 $("#book-search-results").text("Redirecting to the page...");document.location=baseURL+"maps/"+mapid;});});
